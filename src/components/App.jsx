fa10aaacf6a9eca84f1528a0a6cd40d187214901
Mozzhakov <imozzhakov@icloud.com>
2023-03-12 15:09:23 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,3 @@
-import { useEffect, useState } from 'react';
-import { nanoid } from 'nanoid';
 import { ContactForm } from './ContactForm';
 import { ContactList } from './ContactList';
 import { Filter } from './Filter';
